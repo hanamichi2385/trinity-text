ab5cb8fdf68de92456275b97b003d9a2976a2f3b
hanamichi2385 <master2385@hotmail.com>
2022-12-29 16:04:17 +0100

    - Fix consumer definitions

--- Core/TrinityText.ServiceBus.MassTransit/Definitions/GenerateWebsiteConsumerDefinition.cs
+++ Core/TrinityText.ServiceBus.MassTransit/Definitions/GenerateWebsiteConsumerDefinition.cs
@@ -1,4 +1,5 @@
 ﻿using MassTransit;
+using Microsoft.Extensions.Configuration;
 using System;
 using TrinityText.ServiceBus.MassTransit.Consumers;
 
@@ -6,16 +7,26 @@ namespace TrinityText.ServiceBus.MassTransit.Definitions
 {
     public class GenerateWebsiteConsumerDefinition : ConsumerDefinition<GenerateWebsiteConsumer>
     {
-        public GenerateWebsiteConsumerDefinition()
+        private readonly int _retry;
+
+        private readonly int _retryIntervalMinutes;
+        public GenerateWebsiteConsumerDefinition(IConfiguration configuration)
         {
+            var options = configuration.GetSection("MassTransit");
+            var concurrentLimit = options.GetValue<int>("ConcurrentLimit");
+            var retry = options.GetValue<int>("Retry");
+            var retryIntervalMinutes = options.GetValue<int>("RetryIntervalMinutes");
+
             EndpointName = "generate_queue";
-            ConcurrentMessageLimit = 1;
+            ConcurrentMessageLimit = concurrentLimit;
+            _retry = retry;
+            _retryIntervalMinutes = retryIntervalMinutes;
         }
 
         protected override void ConfigureConsumer(IReceiveEndpointConfigurator endpointConfigurator,
             IConsumerConfigurator<GenerateWebsiteConsumer> consumerConfigurator)
         {
-            endpointConfigurator.UseMessageRetry(r => r.Interval(5, TimeSpan.FromMinutes(10)));
+            endpointConfigurator.UseMessageRetry(r => r.Interval(_retry, TimeSpan.FromMinutes(_retryIntervalMinutes)));
             endpointConfigurator.UseInMemoryOutbox();
         }
     }
