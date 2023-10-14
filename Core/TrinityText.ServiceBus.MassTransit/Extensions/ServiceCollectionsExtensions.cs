3a0a5f798614572939010c28eea07c3525a8f9e7
hanamichi2385 <master2385@hotmail.com>
2023-10-14 12:14:48 +0200

    - Add ServiceCollectionExtensions to MassTransit project

---
+++ Core/TrinityText.ServiceBus.MassTransit/Extensions/ServiceCollectionsExtensions.cs
@@ -0,0 +1,30 @@
+﻿using MassTransit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TrinityText.Business;
+using TrinityText.ServiceBus.MassTransit.Consumers;
+using TrinityText.ServiceBus.MassTransit.Definitions;
+using TrinityText.ServiceBus.MassTransit.Services;
+
+namespace TrinityText.ServiceBus.MassTransit
+{
+    public static class ServiceCollectionsExtensions
+    {
+        public static IServiceCollection WithTrinityMassTransitConsumers(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<PublicationSupportOptions>(opt => configuration.GetSection("PublicationSupportOptions").Bind(opt));
+            services.AddScoped<IPublicationSupportService, MassTransitPublicationSupportService>();
+            services.AddScoped<PublishWebsiteConsumer>();
+            services.AddScoped<GenerateWebsiteConsumer>();
+
+            return services;
+        }
+
+        public static IBusRegistrationConfigurator AddTrinityConsumers(this IBusRegistrationConfigurator bsc)
+        {
+            bsc.AddConsumer<GenerateWebsiteConsumer>(typeof(GenerateWebsiteConsumerDefinition));
+            bsc.AddConsumer<PublishWebsiteConsumer>(typeof(PublishWebsiteConsumerDefinition));
+            return bsc;
+        }
+    }
+}
